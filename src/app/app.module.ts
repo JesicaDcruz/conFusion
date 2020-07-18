fd5d247ee0cf1154165a53bb68c05599fc571e22
Jesica Dcruz <jesicadcruz12@gmail.com>
2020-07-18 18:33:50 +0530

    Component PT 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +24,9 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
     MatListModule
   ],
   providers: [],
