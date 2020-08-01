323b281eaf6bdc3e8d6d1e97f524e4f8e882f8f9
Jesica Dcruz <jesicadcruz12@gmail.com>
2020-08-01 17:59:15 +0530

    Promises Pt 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,6 +38,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -65,6 +67,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatCheckboxModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule
